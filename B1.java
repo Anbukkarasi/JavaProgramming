7751d7a2e7d149e48cd42b44fda44f05a8538811
Anbukkarasi <38747280+Anbukkarasi@users.noreply.github.com>
2019-06-17 22:04:44 +0530

    Create B1.java

---
+++ B1.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    char s= 'v';
+    if((s>'a' && s<'z') | (s<'A' && s>='z'))
+    {
+       System.out.println("Alphabet");
+    }
+    else{
+       System.out.println("No");
+    }
+    
+   
+  }
+}
