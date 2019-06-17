8fad89653730a78a8b20c1b28d48b08d94354780
Anbukkarasi <38747280+Anbukkarasi@users.noreply.github.com>
2019-06-17 22:09:18 +0530

    Create B6.java

---
+++ B6.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int a = 2048;
+    if(a%4==0)
+    {
+       System.out.println("yes");
+    }
+    else{
+       System.out.println("no");
+    }
+    
+   
+  }
+}
