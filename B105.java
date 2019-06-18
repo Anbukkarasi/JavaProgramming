5761738253989f2f8e74c6c0714d52e7fd319008
Anbukkarasi <38747280+Anbukkarasi@users.noreply.github.com>
2019-06-18 11:36:06 +0530

    Create B105.java

---
+++ B105.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    int k,sum=1,n;
+    String str;
+     
+    Scanner s = new Scanner(System.in);
+    //str = s.nextLine();
+    n = s.nextInt();
+    k = s.nextInt();
+    System.out.print(n+""+k);
+  }
+}
