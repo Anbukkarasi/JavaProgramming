2d365912482a2fe8fb3f61698935115d85010b34
Anbukkarasi <38747280+Anbukkarasi@users.noreply.github.com>
2019-06-21 11:53:28 +0530

    Create B15even.java

---
+++ B15even.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    
+    Scanner s = new Scanner(System.in);
+    //str = s.nextLine();
+    int n,k;
+    n = s.nextInt();
+    k = s.nextInt();
+   
+    for(int i=n;i<k;i++)
+    {
+      if(i%2 == 0)
+      {
+        System.out.println(i);
+      }
+    }
+ }
+}
